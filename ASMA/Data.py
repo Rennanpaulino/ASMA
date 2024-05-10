1eef626fe143151883319d5a6d52db71cc460948
Rennan Paulino <105396179+Rennanpaulino@users.noreply.github.com>
2024-05-09 21:12:45 -0300

    Add files via upload

---
+++ ASMA/Data.py
@@ -0,0 +1,42 @@
+#from bs4 import BeautifulSoup 
+#import requests
+#Alternative method
+#def beautifulsoup():
+    #url = "https://sistemasinter.cetesb.sp.gov.br/Ar/php/ar_resumo_hora.php"
+    #html = requests.get(url).content
+    #soup = BeautifulSoup(html, "lxml")
+    #print(soup.prettify())
+    #table = soup.find("table", class_ = "font01")
+    #print(table.prettify())
+import pandas as pd
+import numpy as np
+
+class Data():
+    
+    def getdata(ext):
+        df = pd.read_html('https://sistemasinter.cetesb.sp.gov.br/Ar/php/ar_resumo_hora.php')
+        data = pd.DataFrame(df[4])
+    
+        del data['Unnamed: 1']
+        data.rename(columns={'Unnamed: 0':'Região', 'Unnamed: 2': 'Índice', 'Unnamed: 3': 'Poluente'}, inplace=True)
+    
+        conditions = [
+            (data['Índice'] <=40),
+            (data['Índice'] >40) & (data['Índice'] <=80),
+            (data['Índice'] >80) & (data['Índice'] <=120),
+            (data['Índice'] >120) & (data['Índice'] <=200),
+            (data['Índice'] >200) 
+        ]
+        values = ['Boa', 'Moderada', 'Ruim', 'Muito Ruim', 'Péssima']
+        data['Qualidade'] = np.select(conditions, values)
+    
+        if ext.upper() == 'JSON' :
+            data.to_json('C:/Users/Rennan/Desktop/test.json')
+            print('Exported to .JSON')
+        elif ext.upper() == 'CSV' :
+            data.to_csv('C:/Users/Rennan/Desktop/test.csv')
+            print("Exported to .CSV")
+        else:
+            return data            
+
+    getdata('csv') 
