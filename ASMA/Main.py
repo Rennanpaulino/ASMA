5780787c8394c44b498838364efc5117b07018db
Rennanpaulino <rennanpsousa2003@gmail.com>
2024-05-15 20:30:47 -0300

    bug fix

--- ASMA/Main.py
+++ ASMA/Main.py
@@ -1,13 +1,14 @@
 from kivy.app import App
+#from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-
-
-class Main(App):
-    def build(self):
-        return Home()
-    
 
 class Home(BoxLayout):
     pass
+
+class Main(App):
+    def build(self):
+        self.load_kv('home.kv')
+        return Home()
+    
 
 Main().run()
